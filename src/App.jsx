41f90838ae2bf707efc7c23f0e8634130b6d53fd
AlicjaSzamraj <120415960+AlicjaSzamraj@users.noreply.github.com>
2024-11-14 10:09:47 +0000

    local storage prioblems

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Description from "./components/Description";
 import Options from "./components/Options";
 import Feedback from "./components/Feedback";
 import Notification from "./components/Notification";
 
-("./Notification");
 const App = () => {
   const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+  useEffect(() => {
+    const savedFeedback = JSON.parse(localStorage.getItem("feedback"));
+    if (savedFeedback) {
+      setFeedback(savedFeedback);
+    }
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
   const updateFeedback = (feedbackType) => {
     setFeedback((prevFeedback) => ({
       ...prevFeedback,
@@ -17,6 +25,8 @@ const App = () => {
     setFeedback({ good: 0, neutral: 0, bad: 0 });
   };
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positivePercentage =
+    totalFeedback > 0 ? Math.round((feedback.good / totalFeedback) * 100) : 0;
   return (
     <div>
       {" "}
@@ -26,8 +36,17 @@ const App = () => {
         resetFeedback={resetFeedback}
         totalFeedback={totalFeedback}
       />{" "}
-      {totalFeedback > 0 ? <Feedback feedback={feedback} /> : <Notification />}{" "}
+      {totalFeedback > 0 ? (
+        <Feedback
+          feedback={feedback}
+          totalFeedback={totalFeedback}
+          positivePercentage={positivePercentage}
+        />
+      ) : (
+        <Notification />
+      )}{" "}
     </div>
   );
 };
+
 export default App;
--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-const Feedback = ({ feedback }) => {
+const Feedback = ({ feedback, totalFeedback, positivePercentage }) => {
   return (
     <div>
       <p>Good: {feedback.good}</p>
       <p>Neutral: {feedback.neutral}</p>
       <p>Bad: {feedback.bad}</p>
+      <p>Total Feedback: {totalFeedback}</p>
+      <p>Positive Feedback: {positivePercentage}%</p>
     </div>
   );
 };
