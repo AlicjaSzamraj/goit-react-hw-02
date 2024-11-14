be28f299b8bd39ad77677f4fc4f0950b89648fe2
AlicjaSzamraj <120415960+AlicjaSzamraj@users.noreply.github.com>
2024-11-14 09:55:42 +0000

    step 4 done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import Description from "./components/Description";
 import Options from "./components/Options";
 import Feedback from "./components/Feedback";
+import Notification from "./components/Notification";
 
+("./Notification");
 const App = () => {
   const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
   const updateFeedback = (feedbackType) => {
@@ -11,11 +13,20 @@ const App = () => {
       [feedbackType]: prevFeedback[feedbackType] + 1,
     }));
   };
+  const resetFeedback = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
   return (
     <div>
       {" "}
-      <Description /> <Options updateFeedback={updateFeedback} />{" "}
-      <Feedback feedback={feedback} />{" "}
+      <Description />{" "}
+      <Options
+        updateFeedback={updateFeedback}
+        resetFeedback={resetFeedback}
+        totalFeedback={totalFeedback}
+      />{" "}
+      {totalFeedback > 0 ? <Feedback feedback={feedback} /> : <Notification />}{" "}
     </div>
   );
 };
