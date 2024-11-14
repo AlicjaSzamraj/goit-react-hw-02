bfc5b4a6ea04af0368e5c0735e3d93e4511eca1e
AlicjaSzamraj <120415960+AlicjaSzamraj@users.noreply.github.com>
2024-11-14 09:38:45 +0000

    first and second step done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import Description from "./components/Description";
+import Options from "./components/Options";
+import Feedback from "./components/Feedback";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
 
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Description />
+      <Options feedback={feedback} setFeedback={setFeedback} />
+      <Feedback feedback={feedback} />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Options.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+  return (
+    <div>
+      <button onClick={() => updateFeedback("good")}>Good</button>
+      <button onClick={() => updateFeedback("neutral")}>Neutral</button>
+      <button onClick={() => updateFeedback("bad")}>Bad</button>
+    </div>
+  );
+
+export default Options;
